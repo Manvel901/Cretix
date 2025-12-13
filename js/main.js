7f118c30c01ecf2259904f7a99bef7ffa664b412
Manvel <Manvelnavoan@gmail.com>
2025-12-13 23:13:29 +0300

    Completed css Descriptions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 $(function () {
-    // var mixer = mixitup('.directions_items')
-    
+    var mixer = mixitup('.directions_list')
+    $('.directions_link-btn').on('click', function () {
+        $('.directions_link-btn ').removeClass('directions_filter-btn--active')
+        $(this).addClass('directions_filter-btn--active')
+    } )
 })
+
+
+$(document).ready(function () {
+    var mixer = mixitup('.directions_list');
+
+    // Изначально устанавливаем активный класс для первой кнопки
+    $('.directions_filter-btn:first').addClass('active');
+
+    $('.directions_filter-btn').on('click', function () {
+        // Удаляем класс active у всех кнопок
+        $('.directions_filter-btn').removeClass('active');
+        
+        // Добавляем класс active к нажатой кнопке
+        $(this).addClass('active');
+        
+        // Получаем фильтр из data-атрибута
+        var filterValue = $(this).attr('data-filter');
+        
+        // Применяем фильтрацию с MixItUp
+        mixer.filter(filterValue);
+    });
+});
