e19a3933aec6b9aad22c67de16ef1fb8c1e5c152
Manvel <Manvelnavoan@gmail.com>
2025-12-20 20:08:58 +0300

    Completed section team

--- js/main.js
+++ js/main.js
@@ -27,3 +27,33 @@ $(document).ready(function () {
         mixer.filter(filterValue);
     });
 });
+
+
+    // Инициализация Slick Slider
+$(document).ready(function(){
+    $('.team_slider').slick({
+        dots: false, // Убираем точки, если не нужны
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3, // Показываем 3 слайда
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+  
